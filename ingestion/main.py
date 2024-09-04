99a4d4c1329f20c59845ed8db0bdd99f2838eddc
Nahum Sá <nahumsaa@gmail.com>
2024-09-04 14:19:04 -0300

    fix: add debuging for file content

--- ingestion/main.py
+++ ingestion/main.py
@@ -4,4 +4,7 @@ from ingestion.files import FilesExtractor
 if __name__ == "__main__":
     for file in FilesExtractor().extract():
         print(file.name)
-        print(file.sections)
+        for section in file.sections:
+            print()
+            print(section.name)
+            print(section.content)
